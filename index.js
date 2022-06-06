8ef9910c6d437c2b7d19ec23007a2b456ef60dd3
Evans Rutto <robbyevans001@gmail.com>
2022-06-06 13:56:21 +0300

    commit tree 9bcc6449138bd922f0da70a08048e14077bfe343

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+let currentUser = 'Grace Hopper';
+
+let welcomeMessage= 'Welcome to Flatbook, '+currentUser+'!';
+
+let excitedWelcomeMessage=`WELCOME TO FLATBOOK, +${currentUser.toUpperCase()}!`;
+
+// let shortGreeting='Welcome, '+currentUser[0]+currentUser.slice(1)+'!';
+
+let shortGreeting=`Welcome, ${currentUser[0]}!`;
+
+
